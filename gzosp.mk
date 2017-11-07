b80cbbb54efd36c47ba680c6305689c74215a7e9
hardwareadictos <hardwareadictos@gmail.com>
2017-11-07 20:40:42 +0100

    mako: GZOSP-PARTY

---
+++ gzosp.mk
@@ -0,0 +1,21 @@
+# Copyright (C) 2017 The Ground Zero Open Source Project
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/gzosp/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/lge/mako/full_mako.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := mako
+PRODUCT_NAME := gzosp_mako
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 4
+PRODUCT_MANUFACTURER := LGE
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+   PRODUCT_NAME=occam BUILD_FINGERPRINT=google/occam/mako:5.1.1/LMY48M/2167285:user/release-keys \
+   PRIVATE_BUILD_DESC="occam-user 5.1.1 LMY48M 2167285 release-keys" 
